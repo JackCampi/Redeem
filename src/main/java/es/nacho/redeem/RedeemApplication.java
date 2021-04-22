98c0c7c7726291abc6805a0c70ddb2bdd7237ff8
Campi <55862337+JackCampi@users.noreply.github.com>
2021-04-22 15:51:49 -0500

    initial project

---
+++ src/main/java/es/nacho/redeem/RedeemApplication.java
@@ -0,0 +1,13 @@
+package es.nacho.redeem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedeemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedeemApplication.class, args);
+    }
+
+}
