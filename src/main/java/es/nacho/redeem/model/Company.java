a8c7fff49e4791c3a3d55f7a5e18f8a4c8bc7c95
Marlon Barreto <mbarretot@unal.edu.co>
2021-04-26 14:41:19 -0500

    Added another constructor without areas

--- src/main/java/es/nacho/redeem/model/Company.java
+++ src/main/java/es/nacho/redeem/model/Company.java
@@ -1,6 +1,7 @@
 package es.nacho.redeem.model;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.Collection;
 
 @Entity
@@ -38,6 +39,13 @@ public class Company {
         this.name = name;
         this.budget = budget;
         this.areas = areas;
+    }
+
+    public Company(Long id, String name, Long budget) {
+        this.id = id;
+        this.name = name;
+        this.budget = budget;
+        this.areas = new ArrayList<>();
     }
 
     public Company() {
