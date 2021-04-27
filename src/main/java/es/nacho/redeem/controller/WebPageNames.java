b85014ffa45b9171336b0241c78478248a922abb
sashi <sashita2001@hotmail.com>
2021-04-27 11:20:21 -0500

    registerCompany view and registerAdmin view were added, within their own css files

--- src/main/java/es/nacho/redeem/controller/WebPageNames.java
+++ src/main/java/es/nacho/redeem/controller/WebPageNames.java
@@ -1,8 +1,8 @@
 package es.nacho.redeem.controller;
 
 public interface WebPageNames {
-    String ADMIN_REGISTRATION_FORM = "reg_admin";
-    String COMPANY_REGISTRATION_FORM = "reg_temp";
+    String ADMIN_REGISTRATION_FORM = "registerAdmin";
+    String COMPANY_REGISTRATION_FORM = "registerCompany";
     String ADMIN_DASHBOARD = "adminDashboard";
     String EMPLOYEE_REGISTRATION_FORM = "employeeRegistrationForm";
     String EMPLOYEE_DASHBOARD = "employeeDashboard";
