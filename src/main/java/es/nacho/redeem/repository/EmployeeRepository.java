4d6a637cad64dc86e8438345c0ad1b56f852b359
Marlon Barreto <mbarretot@unal.edu.co>
2021-05-13 18:01:31 -0500

    added the method to fetch all employees in an area

--- src/main/java/es/nacho/redeem/repository/EmployeeRepository.java
+++ src/main/java/es/nacho/redeem/repository/EmployeeRepository.java
@@ -1,12 +1,16 @@
 package es.nacho.redeem.repository;
 
+import es.nacho.redeem.model.Area;
 import es.nacho.redeem.model.Employee;
+import es.nacho.redeem.model.compositeKeys.AreaKey;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     Employee findByEmail(String email);
-
+    Collection<Employee> findAllByArea(Area area);
 }
