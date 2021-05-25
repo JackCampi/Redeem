b484c36125e655f1fcaa4b308cddcf9ea3fd5bd5
jmorenohj <jmorenohj@users.noreply.github.com>
2021-05-25 11:45:53 -0500

    Validaciones chidas

--- src/main/resources/static/scripts/transferLogic.js
+++ src/main/resources/static/scripts/transferLogic.js
@@ -17,38 +17,63 @@ var invalidUser = document.getElementById("email_error");
 
 var invalidAmount = document.getElementById("amount_error");
 
+var benValue;
+var amountValue;
 btn.onclick = function(){
-    modal.style.display="block";
-    var benValue = beneficiary.value;
-    var amValue = amount.value;
-    var confText = document.getElementsByClassName("confirm")[0];
-    confText.innerHTML = "Estás seguro de que deseas transferir "+amValue+" a "+benValue+"??";
-}
+    benValue = beneficiary.value;
+    amountValue = amount.value;
+    invalidUser.style.display = "none";
+    invalidAmount.style.display = "none";
 
-
-form.addEventListener('submit', (e) => {
-    var benValue = beneficiary.value;
-    var amountValue = amount.value;
-
-    if((/^\d+$/.test(benValue))){
+    if(benValue.length==0 || amount.length==0){
+        invalidAmount.innerText = "Debe rellenar todos los campos"
+        amount.style.border = '1px solid firebrick';
+        beneficiary.style.border = '1px solid firebrick';
+        amount.focus();
         modal.style.display = "none";
-    }else if (!(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(beneficiary.value))){
+        invalidAmount.style.display = "block";
+        invalidAmount.style.color = "firebrick";
+    }else if (!(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(benValue)) && !(/^\d+$/.test(benValue))){
         beneficiary.style.border = '1px solid firebrick';
         beneficiary.focus();
         modal.style.display = "none";
+        invalidUser.innerText = "Usuario no válido"
         invalidUser.style.display = "block";
         invalidUser.style.color = "firebrick";
-        e.preventDefault()
+
     }else if(parseInt(amountValue) < 10000){
+        invalidAmount.innerText = "La cantidad mínima que se puede transferir es $10.000";
         amount.style.border = '1px solid firebrick';
         amount.focus();
         modal.style.display = "none";
         invalidAmount.style.display = "block";
         invalidAmount.style.color = "firebrick";
-        e.preventDefault();
-    }else{
+    }else if(parseInt(amountValue) > 10000000){
+        invalidAmount.innerText = "La cantidad máxima que se puede transferir es $10'000.000"
+        amount.style.border = '1px solid firebrick';
+        amount.focus();
         modal.style.display = "none";
+        invalidAmount.style.display = "block";
+        invalidAmount.style.color = "firebrick";
+    }else if(!(/^\d+$/.test(amountValue))){
+        invalidAmount.innerText = "Se deben ingresar solo valores numéricos";
+        amount.style.border = '1px solid firebrick';
+        amount.focus();
+        modal.style.display = "none";
+        invalidAmount.style.display = "block";
+        invalidAmount.style.color = "firebrick";
+    } else{
+        modal.style.display="block";
+        var benValue = beneficiary.value;
+        var amValue = amount.value;
+        var confText = document.getElementsByClassName("confirm")[0];
+        confText.innerHTML = "Estás seguro de que deseas transferir $"+amValue+" a "+benValue+"??";
     }
+}
+
+
+form.addEventListener('submit', (e) => {
+    modal.style.display = "none";
 })
 
 
