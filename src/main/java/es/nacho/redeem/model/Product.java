caf8292a6286a4843adc8f6a834eecb8bcbc60a5
Marlon Barreto <mbarretot@unal.edu.co>
2021-06-23 19:50:04 -0500

    Product table was modified and improved

--- src/main/java/es/nacho/redeem/model/Product.java
+++ src/main/java/es/nacho/redeem/model/Product.java
@@ -1,10 +1,14 @@
 package es.nacho.redeem.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.Collection;
 
 @Entity
 @Table(name = "product")
+@Getter @Setter
+@NoArgsConstructor
 public class Product {
 
     @Id
@@ -30,6 +34,9 @@ public class Product {
     @Column(name = "prod_image_url", nullable = false, columnDefinition = "TEXT")
     private String imageUrl;
 
+    @Column(name = "prod_details", nullable = false, columnDefinition = "TEXT")
+    private String details;
+
     @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "product")
     private Collection<PurchaseHasProduct> purchaseHasProducts;
 
@@ -37,82 +44,14 @@ public class Product {
     @JoinColumn(name = "company_comp_id", nullable = false)
     private Company company;
 
-    public Product(String name, Long price, String type, Integer stock, Boolean available, String imageUrl, Company company) {
+    public Product(String name, Long price, String type, Integer stock, String imageUrl, String details, Company company) {
         super();
         this.name = name;
         this.price = price;
         this.type = type;
         this.stock = stock;
-        this.available = available;
         this.imageUrl = imageUrl;
-        this.company = company;
-    }
-
-    public Product() {
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getPrice() {
-        return price;
-    }
-
-    public void setPrice(Long price) {
-        this.price = price;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public Integer getStock() {
-        return stock;
-    }
-
-    public void setStock(Integer stock) {
-        this.stock = stock;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    public Boolean getAvailable() {
-        return available;
-    }
-
-    public void setAvailable(Boolean available) {
-        this.available = available;
-    }
-
-    public Company getCompany() {
-        return company;
-    }
-
-    public void setCompany(Company company) {
+        this.details = details;
         this.company = company;
     }
 }
