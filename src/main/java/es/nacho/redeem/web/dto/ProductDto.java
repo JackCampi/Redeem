4ad805d711a868796525415555d599fcb516dd99
Sebastián <sebas01pq@gmail.com>
2021-06-26 16:14:36 -0500

    Added "description" parameter to ProductDto

--- src/main/java/es/nacho/redeem/web/dto/ProductDto.java
+++ src/main/java/es/nacho/redeem/web/dto/ProductDto.java
@@ -7,14 +7,16 @@ public class ProductDto {
     private Integer stock;
     private Boolean available;
     private String imageUrl;
+    private String description;
 
-    public ProductDto(String name, Long price, String type, Integer stock, Boolean available, String imageUrl) {
+    public ProductDto(String name, Long price, String type, Integer stock, Boolean available, String imageUrl, String description) {
         this.name = name;
         this.price = price;
         this.type = type;
         this.stock = stock;
         this.available = available;
         this.imageUrl = imageUrl;
+        this.description = description;
     }
 
     public ProductDto() {
@@ -66,5 +68,13 @@ public class ProductDto {
 
     public void setImageUrl(String imageUrl) {
         this.imageUrl = imageUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
