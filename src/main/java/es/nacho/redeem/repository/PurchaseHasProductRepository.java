3e92dd4147c75b99103d2d4fa3ada4f476d23651
Sebastián <sebas01pq@gmail.com>
2021-06-26 16:18:29 -0500

    Added Repository annotations to PurchaseHasProductRepository

--- src/main/java/es/nacho/redeem/repository/PurchaseHasProductRepository.java
+++ src/main/java/es/nacho/redeem/repository/PurchaseHasProductRepository.java
@@ -1,4 +1,10 @@
 package es.nacho.redeem.repository;
 
-public interface PurchaseHasProductRepository {
+import es.nacho.redeem.model.PurchaseHasProduct;
+import es.nacho.redeem.model.compositeKeys.PurchaseHasProductKey;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PurchaseHasProductRepository extends JpaRepository<PurchaseHasProduct, PurchaseHasProductKey> {
 }
