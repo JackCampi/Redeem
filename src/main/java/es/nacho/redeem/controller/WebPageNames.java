301bba64205a7429400a57de0308f08aeca09072
Santiago Sánchez Mora <sashita2001@hotmail.com>
2021-06-29 20:11:49 -0500

    Employee dashboard modified, soppingCart added, yet testing but almost there

--- src/main/java/es/nacho/redeem/controller/WebPageNames.java
+++ src/main/java/es/nacho/redeem/controller/WebPageNames.java
@@ -22,6 +22,6 @@ public interface WebPageNames {
     String PRODUCTS_LIST = "adminProductList";
     String PRODUCT_DETAILS = "adminProductDetails";
     String MARKETPLACE = "employeeMarketplace";
-    String EMPLOYEE_CART = "employeeCart";
+    String EMPLOYEE_CART = "shoppingCart";
     String ADMIN_STATISTICS = "adminStatistics";
 }
